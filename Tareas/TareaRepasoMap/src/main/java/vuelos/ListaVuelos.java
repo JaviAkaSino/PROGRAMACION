78634add3692e385189f9c876d37779a45397e92
JaviAkaSino <javiakasino@hotmail.com>
2022-04-01 23:03:56 +0200

    Copia tarea

---
+++ Tareas/TareaRepasoMap/src/main/java/vuelos/ListaVuelos.java
@@ -0,0 +1,110 @@
+package vuelos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author JaviA
+ */
+public class ListaVuelos {
+
+    public static void main(String[] args) {
+
+        final int NUMERO_VUELOS = 10; //Elegimos el número de vuelos random
+        ArrayList<Vuelo> lista = new ArrayList<>();
+
+        for (int i = 0; i < NUMERO_VUELOS; i++) { //Se crean y añaden a la lista
+
+            lista.add(new Vuelo());
+
+        }
+
+        lista.add(new Vuelo("123456")); //Añade uno con el código para poder buscarlo
+
+        for (Object o : lista) { //Imprime lista
+
+            System.out.println(o);
+
+        }
+
+        System.out.println("\n\nPasajeros por destino:");
+        System.out.println(pasajerosPorDestino(lista));
+
+        System.out.println("\n\nPasajeros por destino en orden alfabético:");
+        System.out.println(pasajerosPorDestinoOrden(lista));
+
+        System.out.println("\n\nPasajeros del vuelo 123456: ");
+        System.out.println(pasajerosVuelo(lista, "123456"));
+
+    }
+
+    /*Método estatico que devuelve un MAP que va a contener para cada destino 
+        el nº de pasajeros que llegan en total*/
+    public static Map<String, Integer> pasajerosPorDestino(ArrayList<Vuelo> listaVuelos) {
+
+        Map<String, Integer> vuelosDestino = new HashMap<>(); //Crea el map vacio
+
+        for (Vuelo v : listaVuelos) { //Recorre todos los vuelos
+
+            int n = v.getPasajeros().size(); //Guarda el número de pasajeros
+
+            for (Vuelo b : listaVuelos) { //Recorre en busca de más viajes de i gual destino
+
+                if (v != b && v.getDestino().equals(b.getDestino())) { //Si no es el mismo pero igual destino
+
+                    n += b.getPasajeros().size(); //Se añade la cantidad de pasajeros
+                }
+            }
+
+            vuelosDestino.put(v.getDestino().toString(), n); //Añade elemento al map con (CIUDAD, pasajeros)
+
+        }
+
+        return vuelosDestino;
+    }
+
+    //Repetir metodo pero map ordenado por destinos. Funciona como el anterior
+    public static Map<String, Integer> pasajerosPorDestinoOrden(ArrayList<Vuelo> listaVuelos) {
+
+        Map<String, Integer> vuelosDestino = new TreeMap<>(); //Pero TreeMap en vez de HashMap
+
+        for (Vuelo v : listaVuelos) {
+
+            int n = v.getPasajeros().size();
+
+            for (Vuelo b : listaVuelos) {
+
+                if (v != b && v.getDestino().equals(b.getDestino())) {
+
+                    n += b.getPasajeros().size();
+                }
+            }
+
+            vuelosDestino.put(v.getDestino().toString(), n);
+
+        }
+
+        return vuelosDestino;
+    }
+
+    /*Método que me permita saber por codigo de vuelo los pasajeros (lista) que hay 
+        en ese vuelo*/
+    public static ArrayList<Persona> pasajerosVuelo(ArrayList<Vuelo> lista, String codigoVuelo) {
+
+        ArrayList<Persona> pasajeros = new ArrayList<>();
+
+        for (Vuelo v : lista) { //Recorre todos los vuelos
+
+            if (v.getCodigo().equals(codigoVuelo)) { //Para el vuelo con el código indicado
+
+                return v.getPasajeros(); //Devuelve la lista de pasajeros
+            }
+
+        }
+
+        return pasajeros; //Si no está el vuelo, devuelve una lista vacía (no vuelo, no pasajeros)
+    }
+}
---
+++ Tareas/TareaRepasoMap/src/main/java/vuelos/Persona.java
@@ -0,0 +1,130 @@
+package vuelos;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Random;
+
+/**
+ *
+ * @author javiakasino
+ */
+public class Persona {
+
+    private String nombre;
+    private String apellidos;
+    private NIF nif;
+
+    private static int contador = 0;
+
+    public Persona(String nombre, String apellidos, NIF nif) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nif = nif;
+    }
+
+    public Persona() {
+        this.nombre = nombreDefault();
+        this.apellidos = apellidoDefault();
+        this.nif = new NIF();
+
+        contador++;
+    }
+
+    //Devuelve un nombre aleatorio de la lista
+    private String nombreDefault() {
+
+        String[] lista = {"Juan", "Carlos", "Antonio", "Gabriel",
+            "Eduardo", "Imad", "Javier", "Nacho", "Victoria", "Alvaro"};
+
+        return lista[numeroRandomEntre(0, lista.length - 1)];
+    }
+
+    //Devuelve un apellido aleatorio de la lista
+    private String apellidoDefault() {
+
+        String[] lista = {"García", "Sánchez", "Ruiz", "Pérez",
+            "Martínez", "Martín", "González", "Camacho", "Piña", "Fernández"};
+
+        return lista[numeroRandomEntre(0, lista.length - 1)];
+    }
+
+    public void renovarNIF(LocalDate fechaSolicitud) {
+
+        this.nif.renovar(fechaSolicitud);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public NIF getNif() {
+        return nif;
+    }
+
+    public void setNif(NIF nif) {
+        this.nif = nif;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellidos + "\t" + nif + "\n";
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.nif);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.nif, other.nif)) {
+            return false;
+        }
+        return true;
+    }
+
+    //Da un entero aleatorio dado el intervalo
+    public static int numeroRandomEntre(int valorMin, int valorMax) {
+
+        Random numeroAleatorio = new Random();
+        int aleatorio;
+
+        try {
+
+            aleatorio = numeroAleatorio.nextInt(valorMax - valorMin + 1) + valorMin;
+
+        } catch (IllegalArgumentException iae) {
+
+            aleatorio = numeroAleatorio.nextInt(valorMin - valorMax + 1) + valorMax;
+
+        }
+
+        return aleatorio;
+
+    }
+
+}
